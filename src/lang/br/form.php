8a860fa179ab606681a13e297b0652e5bdcf0b51
willmkt <will.mkt.cba@gmail.com>
2014-07-04 10:11:31 -0400

    Added Brazilian translation

---
+++ src/lang/br/form.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * This file is part of the KraftHaus Bauhaus package.
+ *
+ * (c) KraftHaus <hello@krafthaus.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+
+	'title' => [
+		'edit-model'   => 'Editar :model',
+		'create-model' => 'Criar novo :model'
+	],
+
+	'button' => [
+		'back-to-index' => 'Voltar para :model',
+		'save'          => 'Salvar :model'
+	],
+
+	'modal' => [
+		'loading' => 'Carregando...'
+	]
+
+];
