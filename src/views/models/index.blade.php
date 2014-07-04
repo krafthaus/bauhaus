5ae84fd13b20eece8042ce8363cf71188e08b501
Jelle Spekken <jelle.spekken@centagon.com>
2014-07-04 13:20:00 +0200

    rel=external removed, no need for it

--- src/views/models/index.blade.php
+++ src/views/models/index.blade.php
@@ -116,7 +116,7 @@
 											Export <span class="caret"></span>
 										</button>
 										<ul class="dropdown-menu" role="menu">
-											<li><a href="{{ route('admin.model.export', [$name, 'json']) }}" rel="external">JSON</a></li>
+											<li><a href="{{ route('admin.model.export', [$name, 'json']) }}">JSON</a></li>
 <!--											<li><a href="{{ route('admin.model.export', [$name, 'xml']) }}">XML</a></li>-->
 											<li><a href="{{ route('admin.model.export', [$name, 'csv']) }}">CSV</a></li>
 <!--											<li><a href="{{ route('admin.model.export', [$name, 'xls']) }}">XLS</a></li>-->
