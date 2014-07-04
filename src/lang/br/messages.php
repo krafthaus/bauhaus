d2822cc1a8760ddce5f243448274cc9183e1af29
willmkt <will.mkt.cba@gmail.com>
2014-07-04 10:11:55 -0400

    Create messages.php

---
+++ src/lang/br/messages.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * This file is part of the KraftHaus Bauhaus package.
+ *
+ * (c) KraftHaus <hello@krafthaus.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+
+	'success' => [
+		'title'         => 'Sucesso!',
+		'model-created' => ':model criado',
+		'model-updated' => ':model atualizado',
+		'model-deleted' => 'Deletado :count :model'
+	],
+
+	'warning' => [
+		'title' => 'Atenção!'
+	],
+
+	'error' => [
+		'title'              => 'Whoops!',
+		'validations-errors' => 'Validation errors'
+	]
+
+];
