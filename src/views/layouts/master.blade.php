97eaba5f0406f11498f8c7ecf28b0049afce1513
Jelle Spekken <jelle.spekken@centagon.com>
2014-07-11 10:42:16 +0200

    fluid header update

--- src/views/layouts/master.blade.php
+++ src/views/layouts/master.blade.php
@@ -21,7 +21,7 @@
 
 	<header>
 		<nav class="navbar navbar-default navbar-fixed-top">
-			<div class="container">
+			<div class="container-fluid">
 				<div class="navbar-header">
 					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
 						<span class="sr-only">Toggle navigation</span>
