e1778edf6ee2db2129eb07d1d59427f1e405b6d5
Jelle Spekken <jspekken@gmail.com>
2014-07-28 23:25:00 +0200

    comments added

--- src/KraftHaus/Bauhaus/Result/BaseResult.php
+++ src/KraftHaus/Bauhaus/Result/BaseResult.php
@@ -14,31 +14,75 @@ namespace KraftHaus\Bauhaus\Result;
 class BaseResult
 {
 
+	/**
+	 * Holds the item identifier (normally the id).
+	 * @var integer
+	 */
 	protected $identifier;
+
+	/**
+	 * Holds the resources fields.
+	 * @var array
+	 */
 	protected $fields = [];
 
+	/**
+	 * Set the identifier.
+	 * 
+	 * @param  integer $identifier
+	 *
+	 * @access public
+	 * @return BaseResult
+	 */
 	public function setIdentifier($identifier)
 	{
 		$this->identifier = $identifier;
 		return $this;
 	}
 
+	/**
+	 * Get the identifier.
+	 * 
+	 * @access public
+	 * @return integer
+	 */
 	public function getIdentifier()
 	{
 		return $this->identifier;
 	}
 
+	/**
+	 * Add a field to the result.
+	 * 
+	 * @param  string $key
+	 * @param  string field
+	 *
+	 * @access public
+	 * @return BaseResult
+	 */
 	public function addField($key, $field)
 	{
 		$this->fields[$key] = $field;
 		return $this;
 	}
 
+	/**
+	 * Get the fields.
+	 * 
+	 * @access public
+	 * @return array
+	 */
 	public function getFields()
 	{
 		return $this->fields;
 	}
 
+	/**
+	 * Get a field.
+	 * 
+	 * @access public
+	 * @return string
+	 */
 	public function getField($key)
 	{
 		return $this->fields[$key];
