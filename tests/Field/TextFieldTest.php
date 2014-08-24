53f16f50cc28e4eda929ab0e8b094d1178146605
Jelle Spekken <jspekken@gmail.com>
2014-08-25 00:11:58 +0200

    initial string and text field tests added

---
+++ tests/Field/TextFieldTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace KraftHaus\Bauhaus\Tests;
+
+/**
+ * This file is part of the KraftHaus Bauhaus package.
+ *
+ * (c) KraftHaus <hello@krafthaus.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Orchestra\Testbench\TestCase;
+
+/**
+ * Class TextFieldTest
+ * @package KraftHaus\Bauhaus\Tests
+ */
+class TextFieldTest extends TestCase
+{
+
+}
