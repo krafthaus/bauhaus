85fd3a0a69e175db21db80c9614df515141f85d4
Miguel Vaello <miguel.vaellomartinez@gmail.com>
2014-12-17 16:30:40 +0100

    Added Spanish language support

---
+++ src/lang/es/index.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * This file is part of the KraftHaus Bauhaus package.
+ *
+ * (c) KraftHaus <hello@krafthaus.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+
+	'list-title'        => 'Listar :model',
+	'filter-title'      => 'Buscar por :model',
+	'browsing-filtered' => 'Usted está viendo los resultados de la búsqueda.',
+	'no-filter-results' => 'La búsqueda no ha revuelto ningún resultado.',
+	'no-items-yet'      => 'Todavía no has creado ningún :model.',
+
+	'button' => [
+		'create-new'      => 'Crear nuevo :model',
+		'reset-filters'   => 'Limpar filtros',
+		'edit'            => 'Editar',
+		'delete-selected' => 'Eliminar :model seleccionados',
+		'reset'           => 'Limpar',
+		'filter-submit'   => 'Buscar :model',
+		'overview'        => 'Resumen'
+	],
+
+	'sidebar' => [
+		'scopes' => 'Scopes',
+		'export' => 'Exportar ...'
+	]
+
+];
---
+++ src/lang/es/messages.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * This file is part of the KraftHaus Bauhaus package.
+ *
+ * (c) KraftHaus <hello@krafthaus.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+
+	'success' => [
+		'title'         => 'Sucesso!',
+		'model-created' => ':model creado',
+		'model-updated' => ':model actualizado',
+		'model-deleted' => 'Eliminados :count :model'
+	],
+
+	'warning' => [
+		'title' => '¡Atención!'
+	],
+
+	'error' => [
+		'title'             => '¡Oops!',
+		'validation-errors' => 'Errores de validación'
+	]
+
+];
