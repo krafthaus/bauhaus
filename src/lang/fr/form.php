f633e268c3c6f147a3298b0fecbfb500e73562b6
Jonathan Bonnefoy <contact@e-weap.fr>
2015-01-07 14:50:31 +0100

    Add FR lang

---
+++ src/lang/fr/form.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * This file is part of the KraftHaus Bauhaus package.
+ *
+ * (c) KraftHaus <hello@krafthaus.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+
+	'title' => [
+		'edit-model'   => 'Modifier :model',
+		'create-model' => 'Ajouter un(e) :model'
+	],
+
+	'button' => [
+		'back-to-index' => 'Retour au :model',
+		'save'          => 'Enregistrer :model'
+	],
+
+	'modal' => [
+		'loading' => 'Chargement...'
+	]
+
+];
