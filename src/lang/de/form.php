fce0caa7ab2b5ed555ccd641edc48ae2a33b0a0a
Alexander Mahrt <alex@youngandcreative.de>
2015-01-14 09:12:22 +0100

    Added german translation

---
+++ src/lang/de/form.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * This file is part of the KraftHaus Bauhaus package.
+ *
+ * (c) KraftHaus <hello@krafthaus.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+
+	'title' => [
+		'edit-model'   => ':model bearbeiten',
+		'create-model' => ':model erstellen'
+	],
+
+	'button' => [
+		'back-to-index' => 'Zurück zu :model',
+		'save'          => ':model speichern'
+	],
+
+	'modal' => [
+		'loading' => 'Laden...'
+	]
+
+];
